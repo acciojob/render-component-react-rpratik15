250def22f95623e91426ea10c48e6e1a41ba7fcf
Pratik R Raval <111408819+rpratik15@users.noreply.github.com>
2022-12-24 20:09:41 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Mypara from "./Mypara";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Mypara/>
+    </div>
   )
 }
 
